b0f4d2b23336b05b90a353b716457d67a16f1ce4
Remo de Oliveira Gresta <remoogg@gmail.com>
2021-06-05 19:05:21 -0300

    first commit

---
+++ crw.py
@@ -0,0 +1,50 @@
+import requests
+import json
+
+
+
+BASEURL = "https://api.stackexchange.com/2.2/questions"
+
+
+params = {
+    "site":"stackoverflow"
+}
+
+
+
+# response = requests.get(
+#     'https://api.github.com/search/repositories',
+#     params={'q': 'requests+language:python'},
+# )
+
+
+
+responseQuestion = requests.get(BASEURL,params=params)
+
+responseQuestionItems = responseQuestion.json()['items']
+
+for items in responseQuestionItems:
+    questionId = items['question_id']
+    # print(items['is_answered'])
+    newUrl = BASEURL+'/'+str(questionId)+'/answers'
+
+    responseAnswer = requests.get(newUrl,params=params)
+    responseAnswerItems = responseAnswer.json()['items']
+
+    # print(responseAnswerItems[0]['owner']['user_id'])
+    
+    if(items['is_answered'] == True):
+
+        for aitems in responseAnswerItems:
+            print(aitems['is_accepted'])
+
+
+
+
+
+
+# print(type(responseJson[0])
+# for x in responseJson:
+#     print(x)
+
+
