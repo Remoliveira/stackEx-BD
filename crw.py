a17fabbfc6ff3f856de661a10c61db52b5ae773e
Remo de Oliveira Gresta <remoogg@gmail.com>
2021-06-07 21:47:09 -0300

    minor changes

--- crw.py
+++ crw.py
@@ -1,5 +1,9 @@
 import requests
 import json
+import urllib
+import urllib2
+
+
 
 
 
@@ -26,14 +30,16 @@ responseQuestionItems = responseQuestion.json()['items']
 for items in responseQuestionItems:
     questionId = items['question_id']
     # print(items['is_answered'])
-    newUrl = BASEURL+'/'+str(questionId)+'/answers'
-
-    responseAnswer = requests.get(newUrl,params=params)
-    responseAnswerItems = responseAnswer.json()['items']
+    
 
     # print(responseAnswerItems[0]['owner']['user_id'])
     
     if(items['is_answered'] == True):
+
+        newUrl = BASEURL+'/'+str(questionId)+'/answers'
+
+        responseAnswer = requests.get(newUrl,params=params)
+        responseAnswerItems = responseAnswer.json()['items']
 
         for aitems in responseAnswerItems:
             print(aitems['is_accepted'])
